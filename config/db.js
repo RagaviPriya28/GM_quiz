e95dda1df020ab72392ab103f272ee3b79ff2fbe
= <=>
2024-11-11 14:47:55 +0530

    removed useNewUrlParser and  useUnifiedTopology in database

--- config/db.js
+++ config/db.js
@@ -3,10 +3,7 @@ const dns = require('dns');
 dns.setServers(['8.8.8.8', '8.8.8.4'])
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB Connected');
   } catch (error) {
     console.error('MongoDB connection error:', error.message);
