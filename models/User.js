ab9e060c27199e3cad2fc025544a03e0583d0cfd
= <=>
2024-11-12 11:32:51 +0530

    user model

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: false, unique: true },
+  username: { type: String, required: false},
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   mobile: { type: String, required: true, unique: true },
