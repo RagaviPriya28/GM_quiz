d4e5a3baae794f5a9663c5cd8ac16c56cd1e6aa3
Ashokgmindia <gmi.tn.dev.akmarimuthu@gmail.com>
2024-11-12 18:53:34 +0530

    Server issuse created

--- server.js
+++ server.js
@@ -25,11 +25,41 @@
 //   console.log(`Server running on port ${PORT}`);
 // });
 
+// const app = require('./app');
+// const http = require('http');
+// const connectDB = require('./config/db');
+// const socketHandler = require('./sockets/sockets');
+// const io = require('socket.io'); // Import socket.io
+// const PORT = process.env.PORT || 5000;
+
+// connectDB();
+
+// // Create an HTTP server using the Express app
+// const server = http.createServer(app);
+
+// // Initialize Socket.IO with the HTTP server
+// const socketIo = io(server, {
+//   cors: {
+//     origin: "http://localhost:3000/", // Allow all origins, adjust for production security
+//     methods: ["GET", "POST"]
+//   }
+// });
+
+// // Set socketIo instance on app for later use in controllers
+// app.set('socketio', socketIo); // Make io available in your controllers
+
+// // Pass the io instance to your socket handling function
+// socketHandler(socketIo);
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 const app = require('./app');
 const http = require('http');
 const connectDB = require('./config/db');
 const socketHandler = require('./sockets/sockets');
-const io = require('socket.io'); // Import socket.io
+const socketIo = require('socket.io'); // Import socket.io
 const PORT = process.env.PORT || 5000;
 
 connectDB();
@@ -38,19 +68,16 @@ connectDB();
 const server = http.createServer(app);
 
 // Initialize Socket.IO with the HTTP server
-const socketIo = io(server, {
+const io = socketIo(server, {
   cors: {
-    origin: "*", // Allow all origins, adjust for production security
-    methods: ["GET", "POST"]
-  }
+    origin: "*", // Allow all origins (adjust for production security)
+    methods: ["GET", "POST"],
+  },
 });
 
-// Set socketIo instance on app for later use in controllers
-app.set('socketio', socketIo); // Make io available in your controllers
-
 // Pass the io instance to your socket handling function
-socketHandler(socketIo);
+socketHandler(io);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
