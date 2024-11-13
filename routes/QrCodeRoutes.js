28e1b8ee21bdae1d87ba7bfb2fe2f3e83de38a45
= <=>
2024-11-13 11:33:03 +0530

    qrroutes updated

--- routes/QrCodeRoutes.js
+++ routes/QrCodeRoutes.js
@@ -8,7 +8,7 @@ const { protect, admin } = require('../middlewares/authMiddleware');
 router.get('/generate', protect, admin, QrCodeController.generateQrCode);
 
 // Register user via QR code (changed to accept qrCodeData in URL)
-router.post('/register/:qrCodeData', protect, admin, QrCodeController.registerUser);
+router.post('/register/:qrCodeData', QrCodeController.registerUser);
 
 // List users registered via a specific QR code
 router.get('/registered-users', protect, admin, QrCodeController.getRegisteredUsers);
