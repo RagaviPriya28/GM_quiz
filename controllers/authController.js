836bf32664a7a4f516c4928af1a5d531839f5d86
= <=>
2024-11-18 15:53:56 +0530

    added the expiry time for token

--- controllers/authController.js
+++ controllers/authController.js
@@ -33,7 +33,7 @@ const login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials.' });
 
-    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '8h' });
     res.status(200).json({ token, user });
   } catch (error) {
     res.status(500).json({ message: error.message });
