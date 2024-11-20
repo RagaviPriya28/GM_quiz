605665173165438c21eb37f7559257fac1a4b017
= <=>
2024-11-20 11:48:30 +0530

    changed the auth controlelr api response

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,10 +28,10 @@ const login = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: 'Invalid credentials.' });
+    if (!user) return res.status(400).json({ message: 'Invalid Email.' });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ message: 'Invalid credentials.' });
+    if (!isMatch) return res.status(400).json({ message: 'Invalid Password.' });
 
     const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '8h' });
     res.status(200).json({ token, user });
